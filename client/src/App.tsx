d960cdd850ed6aea6b46525c87962a0b3d8a460b
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-10-30 17:18:58 -0400

    Initial configuration with material theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ReactElement } from 'react';
 import './App.css';
 
-function App() {
+import Button from '@mui/material/Button'
+
+function App(): ReactElement {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div>
+        <Button variant='contained'>oeifuoif</Button>
+      </div>
     </div>
   );
 }
