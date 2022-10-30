c4b23a32f45c5808b7d8c9767d2c733a87151ec2
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-10-29 23:27:51 -0400

    Implemented fetching of data

--- src/services/SryfallFetcher.ts
+++ src/services/SryfallFetcher.ts
@@ -1,12 +1,29 @@
 import axios from 'axios';
+import * as dotenv from 'dotenv';
 import type { DataFetcher } from './CommonInterfaces';
+
+dotenv.config();
 
 export default class ScryfallFetcher implements DataFetcher {
   private searchParams = '';
-	private API_URL = 
 
-  FetchData(input: string): string {
+  private directory = 'cards';
+
+  private queryPrefix = 'search?q';
+
+  private API_URL = process.env['API_SOURCE'];
+
+  FetchData(input: string): Promise<{ total: number; data: any[] } | void> {
     this.searchParams = input;
-    return `YOUR GAVE ME THIS: ${input}`;
+
+    return axios
+      .get(
+        `${this.API_URL}/${this.directory}/${this.queryPrefix}=${this.searchParams}`,
+      )
+      .then((result) => ({
+        total: result.data.total_cards,
+        data: result.data.data,
+      }))
+      .catch((error) => console.error(error));
   }
 }
