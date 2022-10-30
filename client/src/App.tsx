4f1081cd8f4201ea49e4e06ff6bfb736639bac86
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-10-30 19:27:32 -0400

    WIP
    added placeholder for results

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,9 @@ import './App.css';
 
 
 function App(): ReactElement {
+
+  const holder = [1,2,3,4,5,6,7,8, 9, 10, 11, 12]
+
   return (
     <div className="min-h-full App">
   <nav className="bg-gray-800">
@@ -41,14 +44,27 @@ function App(): ReactElement {
   </header>
   <main>
     <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-      <div>
+      <div className="my-10">
         <div className="rounded-md shadow-sm drop-shadow-2xl flex">
           <label htmlFor="searchbox" className='bg-blend-normal'>Search:</label>
           <input type="text" name="searchbox" id="searchbox" className="block w-full rounded-md pl-7 pr-12 focus:border-black-200 focus:ring-indigo-500" placeholder="what's the name of the card you are looking for?" />
         </div>
       </div>
       <div className="px-4 py-6 sm:px-0">
-        <div className="h-96 rounded-lg border-4 border-dashed border-gray-200"></div>
+        <div className="flex flex-wrap">
+          {holder.map(() => (
+            <div className='h-14 h-80 m-5 p-2 flex from-inherit bg-indigo-500 shadow-lg shadow-indigo-500/50 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ' >
+              <img src='https://cards.scryfall.io/png/front/5/6/56b0f66b-dca9-4a01-9394-20a513c2b225.png?1660724670' className="mx-3"/>
+              <div className='flex flex-col'>
+                <div className='text-white text-left'>A-Young Blue Dragon // A-Sand Augury</div>
+                <div className='text-white text-left'>Alchemy Horizons: Baldur's Gate</div>
+                <div className='text-white text-left'>A-138</div>
+                <div className='text-white text-left'>Common</div>
+              </div>
+          </div>
+          ))}
+          
+        </div>
       </div>
     </div>
   </main>
