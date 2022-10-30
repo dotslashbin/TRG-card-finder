6d78a255a16a1229733140c63dceb1e842a392d7
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-10-30 13:15:49 -0400

    Cleaned up use of types

--- src/services/CommonInterfaces.ts
+++ src/services/CommonInterfaces.ts
@@ -1,19 +1,3 @@
-export type SuccessfulAPICall = {
-  object: string;
-  total_cards: number;
-  has_more: boolean;
-  data: any;
-};
-
-export type FailedAPICall = {
-  object: string;
-  code: string;
-  status: number;
-  details: string;
-};
-
 export interface DataFetcher {
-  FetchData(
-    paramters: string,
-  ): Promise<SuccessfulAPICall | FailedAPICall | void>;
+  FetchData(paramters: string): Promise<void>;
 }
