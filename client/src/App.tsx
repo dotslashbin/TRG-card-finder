11cf2491f9c24245fdc311849898534726b95b56
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-10-30 18:21:20 -0400

    Initial layout design up to search bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,58 @@
 import React, { ReactElement } from 'react';
 import './App.css';
 
-import Button from '@mui/material/Button'
 
 function App(): ReactElement {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <div>
-        <Button variant='contained'>oeifuoif</Button>
+    <div className="min-h-full App">
+  <nav className="bg-gray-800">
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="flex h-16 items-center justify-between">
+        <div className="flex items-center">
+          <div className="flex-shrink-0">
+            <img className="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-10 flex items-baseline space-x-4">
+              <a href="#" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
+              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Josh's Github</a>
+              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Linked In</a>
+              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact Him</a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
+
+    <div className="md:hidden" id="mobile-menu">
+      <div className="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+        <a href="#" className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Dashboard</a>
+        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Josh's Github</a>
+        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Linked In</a>
+        <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contact Him</a>
+      </div>
+    </div>
+  </nav>
+
+  <header className="bg-white shadow">
+    <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+      <h1 className="text-3xl font-bold tracking-tight text-gray-900">TCG Card searcher</h1>
+    </div>
+  </header>
+  <main>
+    <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+      <div>
+        <div className="rounded-md shadow-sm drop-shadow-2xl flex">
+          <label htmlFor="searchbox" className='bg-blend-normal'>Search:</label>
+          <input type="text" name="searchbox" id="searchbox" className="block w-full rounded-md pl-7 pr-12 focus:border-black-200 focus:ring-indigo-500" placeholder="what's the name of the card you are looking for?" />
+        </div>
+      </div>
+      <div className="px-4 py-6 sm:px-0">
+        <div className="h-96 rounded-lg border-4 border-dashed border-gray-200"></div>
+      </div>
+    </div>
+  </main>
+</div>
   );
 }
 
