d53d76ee18650207aac79242e229603e2a255ba7
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-10-29 23:42:45 -0400

    Refactored to a cleaner implementation

--- src/handlers/GET.ts
+++ src/handlers/GET.ts
@@ -1,18 +1,19 @@
 import type { Request, Response } from 'express';
-import { ReturnSuccess } from '../helpers/Response';
+import { ReturnError, ReturnSuccess } from '../helpers/Response';
 import ScryfallFetcher from '../services/SryfallFetcher';
 
 // eslint-disable-next-line import/prefer-default-export
-export async function FindCards(_request: Request, response: Response) {
+export async function FindCards(request: Request, response: Response) {
   const service = new ScryfallFetcher();
+  const { query } = request.query;
 
-  ReturnSuccess(
-    200,
-    response,
-    'get-cards',
-    { foo: 'bar', another: service.FetchData('testing lang') },
-    'Successfully fetched something',
-  );
-
-  // return axios.get('https://api.scryfall.com/cards/search?q=blue');
+  await service
+    .FetchData(`${query}`)
+    .then((result) => {
+      ReturnSuccess(200, response, 'get-cards', result, 'success');
+    })
+    .catch((error) => {
+      console.error('TET LANG ', error);
+      ReturnError(400, response, error, 'Failed to fetch data from source');
+    });
 }
