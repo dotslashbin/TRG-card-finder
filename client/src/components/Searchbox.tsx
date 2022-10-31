e4bc988ed767b7709ff67a6b9c01fba79db29abe
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-10-30 20:41:23 -0400

    Created container placeholders

---
+++ client/src/components/Searchbox.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react'
+
+function Searchbox(): ReactElement {
+	return(
+		<div className="rounded-md shadow-sm drop-shadow-2xl flex">
+			<label htmlFor="searchbox" className='bg-blend-normal'>Search:</label>
+			<input type="text" name="searchbox" id="searchbox" className="block w-full rounded-md pl-7 pr-12 focus:border-black-200 focus:ring-indigo-500" placeholder="what's the name of the card you are looking for?" />
+		</div>
+	)
+}
+
+export default Searchbox
