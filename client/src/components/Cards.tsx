00ac8ab2456beb4f6bb79cc783897ada756a7849
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-10-30 23:41:03 -0400

    Updated displaying of pictures

--- client/src/components/Cards.tsx
+++ client/src/components/Cards.tsx
@@ -6,8 +6,8 @@ function Cards(props: { data: any[]  }): ReactElement {
 	return (
 		<div className="flex flex-wrap">
           {data? data.map((card: any, key: number) => (
-            <div key={ key } className='h-14 h-80 m-5 p-2 flex from-inherit bg-indigo-500 shadow-lg shadow-indigo-500/50 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ' >
-              <img src='https://cards.scryfall.io/png/front/5/6/56b0f66b-dca9-4a01-9394-20a513c2b225.png?1660724670' className="mx-3"/>
+            <div key={ key } className='h-14 h-80 min-w-60 max-w-400 m-5 p-2 flex from-inherit bg-indigo-500 shadow-lg shadow-indigo-500/50 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ' >
+              <img src={ card.image_uris.png } className="mx-3"/>
               <div className='flex flex-col'>
                 <div className='text-white text-left'>{ card.name }</div>
                 <div className='text-white text-left'>{ card.set_name }</div>
